5ec4aacccae21f221dbbbac60692c2a73c0d3123
Chung <truongmanhchung207@gmail.com>
2021-01-14 11:56:46 +0700

    update song component

--- src/app/songpage/song/song.component.ts
+++ src/app/songpage/song/song.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SongService} from "../../service/song/song.service";
+import {Track} from "ngx-audio-player";
+import {singer} from "../../model/singer";
+import {song} from "../../model/song";
 
 @Component({
   selector: 'app-song',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./song.component.css']
 })
 export class SongComponent implements OnInit {
+  track: Track = {};
+  singers: singer[] = [];
+  song: song = {};
 
-  constructor() { }
+  constructor(private songService: SongService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  getTrackById(id){
+    this.songService.getSongById(id).subscribe(value =>{
+      this.track.title = value.name;
+      this.track.link = value.file;
+      this.singers = value.singers;
+      this.song = value;
+    });
   }
 
 }
