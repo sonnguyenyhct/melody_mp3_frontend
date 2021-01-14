4ad6bd0419c5e37617fae30c7142e9fcd89bc456
Chung <truongmanhchung207@gmail.com>
2021-01-14 14:36:06 +0700

    update song component

--- src/app/songpage/song/song.component.ts
+++ src/app/songpage/song/song.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {SongService} from "../../service/song/song.service";
-import {Track} from "ngx-audio-player";
-import {singer} from "../../model/singer";
-import {song} from "../../model/song";
+import {SongService} from '../../service/song/song.service';
+import {Track} from 'ngx-audio-player';
+import {singer} from '../../model/singer';
+import {song} from '../../model/song';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-song',
@@ -10,20 +11,35 @@ import {song} from "../../model/song";
   styleUrls: ['./song.component.css']
 })
 export class SongComponent implements OnInit {
-  track: Track = {};
+  singleTrack: Track[] = [
+    {
+      link: '',
+      title: ''
+    }
+  ];
   singers: singer[] = [];
-  song: song = {};
+  song: song = {
+    name: '',
+    file: '',
+    singers: []
+  };
+  id: number;
 
-  constructor(private songService: SongService) {
+  constructor(private songService: SongService,
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      this.id = +paramMap.get('id');
+      this.getTrackById(this.id);
+    });
   }
 
   ngOnInit(): void {
   }
 
-  getTrackById(id){
-    this.songService.getSongById(id).subscribe(value =>{
-      this.track.title = value.name;
-      this.track.link = value.file;
+  getTrackById(id) {
+    this.songService.getSongById(id).subscribe(value => {
+      this.singleTrack[0].title = value.name;
+      this.singleTrack[0].link = value.file;
       this.singers = value.singers;
       this.song = value;
     });
