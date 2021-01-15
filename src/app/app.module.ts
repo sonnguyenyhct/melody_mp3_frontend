be273282b923d2e600c0358c8f55a6a8d677b925
conghoan21 <conghoan.hnc@gmail.com>
2021-01-15 17:36:37 +0700

    add file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {NgbCarouselModule, NgbModule} from '@ng-bootstrap/ng-bootstrap';
-<<<<<<< HEAD
 import {FooterComponent} from './shared/footer/footer.component';
 import {HeaderComponent} from './shared/header/header.component';
 import {NavbarComponent} from './shared/navbar/navbar.component';
@@ -22,7 +21,6 @@ import { CreatePlaylistComponent } from './playlist/create-playlist/create-playl
 import { ListPlaylistComponent } from './playlist/list-playlist/list-playlist.component';
 import { UpdatePlaylistComponent } from './playlist/update-playlist/update-playlist.component';
 import { DeletePlaylistComponent } from './playlist/delete-playlist/delete-playlist.component';
-=======
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -32,7 +30,6 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 
->>>>>>> 6e733b7bc8db154bf3707cd20ae8c5e580e57e9b
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,16 +40,14 @@ import { ProfileComponent } from './profile/profile.component';
     HomepageComponent,
     LoginComponent,
     RegisterComponent,
-<<<<<<< HEAD
+
     ProfileComponent,
     SongComponent,
     CreatePlaylistComponent,
     ListPlaylistComponent,
     UpdatePlaylistComponent,
     DeletePlaylistComponent
-=======
     ProfileComponent
->>>>>>> 6e733b7bc8db154bf3707cd20ae8c5e580e57e9b
   ],
   imports: [
     BrowserModule,
