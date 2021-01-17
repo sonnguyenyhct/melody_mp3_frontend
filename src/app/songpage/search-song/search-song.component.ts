09cbc2325ca391f721309acc4d7796bb2c019f19
Chung <truongmanhchung207@gmail.com>
2021-01-18 01:21:09 +0700

    create component searchsong

---
+++ src/app/songpage/search-song/search-song.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {SongService} from "../../service/song/song.service";
+import {ActivatedRoute} from "@angular/router";
+import {song} from "../../model/song";
+
+@Component({
+  selector: 'app-search-song',
+  templateUrl: './search-song.component.html',
+  styleUrls: ['./search-song.component.css']
+})
+export class SearchSongComponent implements OnInit {
+  keyword: string | null;
+  songs: song[] = [];
+
+  constructor(private songService: SongService,
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      this.keyword = paramMap.get('keyword');
+      this.getListSong(this.keyword);
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  getListSong(keyword) {
+    this.songService.findAllByNameContains(keyword).subscribe(value => {
+      this.songs = value;
+    });
+  }
+
+}
