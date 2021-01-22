e42eac95193c027e42a2e301c32c9562c1eea984
Chung <truongmanhchung207@gmail.com>
2021-01-22 10:32:21 +0700

    update component play-playlist

--- src/app/playlist/play-playlist/play-playlist.component.ts
+++ src/app/playlist/play-playlist/play-playlist.component.ts
@@ -3,6 +3,8 @@ import {PlaylistService} from "../../service/playlist/playlist.service";
 import {ActivatedRoute} from "@angular/router";
 import {Track} from "ngx-audio-player";
 import {Playlist} from "../../model/playlist";
+import {LikePlaylist} from "../../model/like-playlist";
+import {AuthService} from "../../service/auth/auth.service";
 
 @Component({
   selector: 'app-play-playlist',
@@ -14,22 +16,36 @@ export class PlayPlaylistComponent implements OnInit {
   idPlaylist: number | undefined;
   trackPlaylists: Track[] = [];
   topPlaylists: Playlist[] = [];
+  typeButtonLikePlaylist: string = 'btn btn-outline-danger';
+  titleLike: string = 'Like';
+  currentUser: any = {};
+  likePLayList: LikePlaylist = {
+    user: this.currentUser,
+    playlist: this.playlist
+  }
 
 
   constructor(private playlistService: PlaylistService,
-              private activatedRoute: ActivatedRoute) {
+              private activatedRoute: ActivatedRoute,
+              private authService: AuthService) {
+    this.authService.currentUserSubject.subscribe(value => {
+      this.likePLayList.user = value;
+      this.currentUser = value;
+    });
     this.activatedRoute.paramMap.subscribe(paramMap => {
       // @ts-ignore
       this.idPlaylist = +paramMap.get('id');
       this.getTrackPlaylist(this.idPlaylist);
       this.getPlaylistById(this.idPlaylist);
+      this.playlist.id = this.idPlaylist;
     })
-    this.playlistService.getPlayListMostLike().subscribe(value => {
+    this.playlistService.getTopView().subscribe(value => {
       this.topPlaylists = value;
     })
   }
 
   ngOnInit(): void {
+    this.checkStatusLikePlaylist(this.likePLayList);
   }
 
   getTrackPlaylist(idPlaylist: number){
@@ -41,7 +57,35 @@ export class PlayPlaylistComponent implements OnInit {
   getPlaylistById(idPlaylist: number){
     this.playlistService.getPlayListById(idPlaylist).subscribe(value => {
       this.playlist = value;
+      this.likePLayList.playlist = value;
     })
   }
+
+  checkStatusLikePlaylist(likePLayList: LikePlaylist){
+    this.playlistService.getLikeStatus(likePLayList).subscribe(value => {
+      if (value == null){
+        this.typeButtonLikePlaylist = 'btn btn-outline-danger';
+        this.titleLike = 'Like';
+      } else {
+        this.typeButtonLikePlaylist = 'btn btn-danger';
+        this.titleLike = 'Unlike';
+      }
+    })
+  }
+
+  changeStatusLike(likePlayList: LikePlaylist){
+    this.playlistService.getLikeStatus(likePlayList).subscribe(value => {
+      if (value == null){
+        this.playlistService.addLikePlaylist(likePlayList).subscribe(value1 => {
+          this.checkStatusLikePlaylist(likePlayList)
+        });
+      } else {
+        this.playlistService.deleteLikePlaylist(likePlayList).subscribe(value1 => {
+          this.checkStatusLikePlaylist(likePlayList);
+        });
+      }
+    })
+  }
+
 
 }
